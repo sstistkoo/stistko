60a9bad0a4666b29ba24f047135693e00297bc1a
stistko <65913642+sstistkoo@users.noreply.github.com>
2026-04-19 16:05:52 +0200

    uprava

---
+++ christ/strong_translate/jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'mjs'],
+  testMatch: ['**/*.test.mjs'],
+  collectCoverageFrom: ['strong_translator_core.js'],
+  transform: {}
+};
